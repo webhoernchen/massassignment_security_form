8e4382d0068dcce0e45968373bc5860e3d36a588
Christian Eichhorn <c.eichhorn@webmasters.de>
2017-09-11 10:06:28 +0200

    * old test_apps removed

--- script/run_tests.sh
+++ script/run_tests.sh
@@ -2,10 +2,12 @@
 
 source "$HOME/.rvm/scripts/rvm"
 
+set -e
+
 local_folder=$(pwd)
 RESULT=0
 
-for test_app in $(ls test_apps)
+for test_app in $(ls test_apps | grep -vE 'rails_(2_3|3_0|3_1|3_2)')
 do
   if [[ $RESULT == 0 ]]
   then
@@ -13,7 +15,7 @@ do
     cd $local_folder/test_apps/$test_app
     rvm use $(cat RUBY_VERSION)
     export BUNDLE_GEMFILE="$local_folder/test_apps/$test_app/Gemfile"
-#    bundle install
+    bundle install
     bundle exec rake db:create:all
     bundle exec rake db:migrate
     bundle exec rake 
