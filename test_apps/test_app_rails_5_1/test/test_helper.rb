72a27f2ace399d0944660e1c45d8388c8052c833
Christian Eichhorn <c.eichhorn@webmasters.de>
2017-09-11 12:14:10 +0200

    * config for rails 5.1 fixed

--- test_apps/test_app_rails_5_1/test/test_helper.rb
+++ test_apps/test_app_rails_5_1/test/test_helper.rb
@@ -4,7 +4,6 @@ require 'rails/test_help'
 
 require File.expand_path(File.dirname(__FILE__) + "/../../../test/test_helper")
 
-p File.expand_path(File.dirname(__FILE__) + "/../../../test/**/*_test.rb")
 Dir[File.expand_path(File.dirname(__FILE__) + "/../../../test/**/*_test.rb")].sort.each do |model|
   require model
 end
