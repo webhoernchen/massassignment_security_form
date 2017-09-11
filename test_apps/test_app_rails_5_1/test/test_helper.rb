81285088b34328022d10ee3c7b767a1499f08ce1
Christian Eichhorn <c.eichhorn@webmasters.de>
2017-09-11 10:10:23 +0200

    * test_app_rails_5_1 added

---
+++ test_apps/test_app_rails_5_1/test/test_helper.rb
@@ -0,0 +1,15 @@
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+require File.expand_path(File.dirname(__FILE__) + "/../../../test/test_helper")
+
+Dir[File.expand_path(File.dirname(__FILE__) + "/../../../test/**/*_test.rb")].sort.each do |model|
+  require model
+end
+
+#class ActiveSupport::TestCase
+#  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+#  fixtures :all
+#
+#  # Add more helper methods to be used by all tests here...
+#end
